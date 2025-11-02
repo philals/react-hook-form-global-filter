7d028567f2dacec6d0726c6d0c2b4e93e83441ce
Phil Alsford <philals@users.noreply.github.com>
2025-11-02 19:18:38 +0000

    ?

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["*.csb.app"],
+    allowedHosts: [".csb.app"],
     port: 3000,
     open: true,
   },
